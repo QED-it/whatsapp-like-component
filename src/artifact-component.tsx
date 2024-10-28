16ec53c49c6efcfac5c6b89847ef7f08c0ed4d29
Jonathan Rouach <jon@arjeo.com>
2024-10-28 16:24:33 +0200

    add verified by qedit

--- src/artifact-component.tsx
+++ src/artifact-component.tsx
@@ -74,6 +74,20 @@ const TicketChangeApp = () => {
     }, 2000);
   };
 
+  const QeditLogo = () => (
+    <svg width="70" height="40" viewBox="0 0 140 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g xmlns="http://www.w3.org/2000/svg" id="Group_1111" data-name="Group 1111" >
+        <path id="Path_633" data-name="Path 633" d="M48.507,2669.484c0-9.044-6.907-15.8-16.26-15.8-9.322,0-16.2,6.752-16.2,15.8s6.876,15.795,16.2,15.795a17.738,17.738,0,0,0,5.642-.922v-8.664a7.886,7.886,0,0,1-5.43,2.059c-4.553,0-7.959-3.406-7.959-8.269s3.407-8.27,7.959-8.27a7.87,7.87,0,0,1,7.99,8.27h0v13.8h0v12.133h8.052v-25.469l-.006,0C48.5,2669.79,48.507,2669.637,48.507,2669.484Z" transform="translate(-16.049 -2643.551)" fill="#fff"/>
+        <path id="Path_634" data-name="Path 634" d="M494.234,2666.456h13.287c-.743-3.841-3.066-6.008-6.535-6.008-3.81,0-6.04,2.292-6.752,6.008m21.4,2.788a23.239,23.239,0,0,1-.155,2.725H494.2c.867,4.336,3.9,6.225,7.712,6.225a12.136,12.136,0,0,0,7.588-2.88l4.739,5.2c-3.407,3.128-7.743,4.552-12.791,4.552-9.2,0-15.578-6.256-15.578-15.7s6.194-15.889,15.176-15.889c8.641,0,14.556,6.412,14.588,15.765" transform="translate(-451.083 -2643.358)" fill="#fff"/>
+        <g id="Group_1110" data-name="Group 1110" transform="translate(104.685 0)">
+          <rect id="Rectangle_193" data-name="Rectangle 193" width="8.084" height="30.819" transform="translate(0 10.118)" fill="#fff"/>
+          <rect id="Rectangle_194" data-name="Rectangle 194" width="8.084" height="7.101" fill="#fff"/>
+        </g>
+        <path id="Path_635" data-name="Path 635" d="M1629.3,2534.1h-9.456v11.771c0,3.748,1.982,5.111,4.491,5.111a11.733,11.733,0,0,0,5.637-1.951l2.974,6.318A16.749,16.749,0,0,1,1623,2558.5c-7.434,0-11.243-4.212-11.243-9.761v-31.95h8.083v10.118h9.456Z" transform="translate(-1493.603 -2516.792)" fill="#fff"/>
+        <path id="Path_636" data-name="Path 636" d="M951.14,2516.792h-8.052v12.133a17.212,17.212,0,0,0-8.207-2c-9.323,0-16.2,6.752-16.2,15.8s6.876,15.795,16.2,15.795c9.353,0,16.26-6.751,16.26-15.795,0-.154,0-.307-.006-.459l.006,0Zm-16.26,34.2c-4.553,0-7.959-3.407-7.959-8.27s3.407-8.269,7.959-8.269a8.274,8.274,0,0,1,0,16.539Z" transform="translate(-851.844 -2516.792)" fill="#fff"/>
+      </g>  
+    </svg>
+  );
   return (
     <div style={{ 
       display: 'flex',
@@ -311,6 +325,20 @@ const TicketChangeApp = () => {
                 {state.confirmationMessage}
               </div>
             )}
+
+            {/* Verification Logo */}
+            <div style={{
+              marginTop: '32px',
+              display: 'flex',
+              alignItems: 'right',
+              justifyContent: 'right',
+              gap: '8px',
+              color: 'white',
+              fontSize: '14px'
+            }}>
+              <span>Verified by</span>
+              <QeditLogo />
+            </div>
           </div>
         </div>
       </div>
